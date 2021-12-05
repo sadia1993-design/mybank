aaeba4dda52788ed0e5c12ecc9699acd7056c131
Nishat Sanjida <56416902+sadia1993-design@users.noreply.github.com>
2021-12-04 23:52:49 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 const loginButton= document.getElementById("login_btn");
  
-loginButton.addEventListener("click", function(){
+loginButton.addEventListener("click", function(e){
+ e.preventDefault();
     const fullArea= document.getElementById("login_area");
     fullArea.style.display="none";
 
